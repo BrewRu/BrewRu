d9222e36fc060dc30da178d0f2ffd1def9ff43c6
Ryan Bonick <rjbman93@gmail.com>
2014-10-28 08:07:51 -0500

    Added index page @threeset3

--- BrewRu/urls.py
+++ BrewRu/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 import recipe
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'BrewRu.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    # Homepage
+    url(r'^$', TemplateView.as_view(template_name="base.html")),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^recipe/', include('recipe.urls')),
