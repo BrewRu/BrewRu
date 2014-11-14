6fa3ef9d1939e40151fc1ddbd661ce78883a1db7
jmartinsiuc <jordanmartin@gmail.com>
2014-11-14 10:39:44 -0600

    remove

---
+++ recipe/migrations/0015_auto_20141110_1657.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recipe', '0014_auto_20141105_1429'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='recipe',
+            name='subtype',
+            field=models.CharField(choices=[('Ale', (('pale ale', 'Pale Ale'), ('ipa', 'IPA'), ('amber', 'Amber'), ('brown', 'Brown'), ('wheat ale', 'Wheat Ale'), ('stout', 'Stout'), ('belgian', 'Belgian'))), ('Lager', (('light lager', 'Light Lager'), ('hybrid lager', 'Hybrid Lager'), ('bock', 'Bock')))], default='wheat ale', max_length=50),
+            preserve_default=True,
+        ),
+    ]
